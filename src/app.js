f69833b9a0493c2ec046609f972f5734119d027e
Ritik <rs830595@gmail.com>
2026-07-21 11:38:05 +0530

    Fix CORS to support credentialed requests

--- src/app.js
+++ src/app.js
@@ -9,7 +9,12 @@ const AppError = require('./utils/AppError');
 
 const app = express();
 
-app.use(cors());
+const corsOptions = {
+  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  credentials: true,
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser());
 
